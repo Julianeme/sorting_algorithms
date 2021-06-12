fb7c7d44f407e1a1c81ec5596cf6172d0f92eb4e
vanemcb <vanem2201@gmail.com>
2021-06-12 11:50:34 -0500

    Function selection_sort created

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm.
+ * @array: array of integers
+ * @size: array size
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, x;
+	int pos, min;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		for (x = i + 1; x < size; x++)
+		{
+			if (array[x] < min)
+			{
+				min = array[x];
+				pos = x;
+			}
+		}
+		if (array[i] != min)
+		{
+			array[pos] = array[i];
+			array[i] = min;
+		}
+		print_array(array, size);
+	}
+}
