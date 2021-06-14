d2d7cf3bed391ee4679401a2a809f11316137c2c
vanemcb <vanem2201@gmail.com>
2021-06-14 09:17:16 -0500

    Header updated

--- sort.h
+++ sort.h
@@ -32,5 +32,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void quick_sort_aux(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
